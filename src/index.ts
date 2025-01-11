258a5277cdb7a7d41a229204772b345c56700d63
hectorpaliza28 <86799919+hectorpaliza28@users.noreply.github.com>
2025-01-10 19:21:17 -0700

    se agrega puerto para deploy en heroku

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import authRoutes from './routes/auth.routes';
 import usuarioRoutes from './routes/usuario.routes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
