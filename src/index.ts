58b5165a549e8fd4a2b73f98b15e69fcf0653d9b
hectorpaliza28 <86799919+hectorpaliza28@users.noreply.github.com>
2025-01-10 19:28:04 -0700

    cleaning index

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,14 @@ import { MainDataSource } from './config/db.config';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import usuarioRoutes from './routes/usuario.routes';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
 
 app.use('/auth', authRoutes);
 app.use('/usuarios', usuarioRoutes);
